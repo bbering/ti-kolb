6f8ba83ec0a8fddaa1860faca50baf56a632080c
bbering <bbering88@gmail.com>
2025-06-10 16:22:57 -0300

    feat(KolbController): criação do metodo POST na controller

--- src/main/java/com/tutor/ti_kolb/controllers/KolbController.java
+++ src/main/java/com/tutor/ti_kolb/controllers/KolbController.java
@@ -1,5 +1,6 @@
 package com.tutor.ti_kolb.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,14 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tutor.ti_kolb.dtos.KolbResultDTO;
+import com.tutor.ti_kolb.dtos.KolbTestRequestDTO;
+import com.tutor.ti_kolb.services.TesteKolbService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/kolb")
 public class KolbController {
-    
-    @PostMapping
-    public ResponseEntity<KolbResultDTO> doTest(){
-        // implementação do metodo post
+
+    private final TesteKolbService testService;
+
+    public KolbController(TesteKolbService testeKolbService) {
+        this.testService = testeKolbService;
+    }
+
+    @PostMapping("/dotest")
+    public ResponseEntity<KolbResultDTO> doTest(@Valid @RequestBody KolbTestRequestDTO kolbTestData) {
+        KolbResultDTO methodAnswer = testService.makeTest(kolbTestData.userId(), kolbTestData.answers());
+        return new ResponseEntity<>(methodAnswer, HttpStatus.CREATED);
     }
 
 }
