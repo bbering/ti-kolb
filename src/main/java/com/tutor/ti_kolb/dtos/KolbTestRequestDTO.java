a4b7079c1ba70b7c2ef024f4eb6f18ed43744c5a
bbering <bbering88@gmail.com>
2025-06-10 16:17:03 -0300

    feat(KolbTestRequestDTO): criação do DTO usado para quando o usuario tentar criar um teste de Kolb, de modo a nao manipular a entity

---
+++ src/main/java/com/tutor/ti_kolb/dtos/KolbTestRequestDTO.java
@@ -0,0 +1,12 @@
+package com.tutor.ti_kolb.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+import java.util.UUID;
+
+public record KolbTestRequestDTO(
+        @NotNull(message = "UserId não pode ser nulo") UUID userId,
+
+        @NotNull(message = "Respostas não podem ser nulas") @Size(min = 48, max = 48, message = "Devem ser exatamente 48 respostas") List<Integer> answers) {
+}
