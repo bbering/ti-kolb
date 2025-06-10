8ffacd8d2cd41e7fd5187171a91ceca382d18a64
bbering <bbering88@gmail.com>
2025-06-10 15:50:03 -0300

    feat(TesteKolbService): criação da service para aplicação

---
+++ src/main/java/com/tutor/ti_kolb/services/TesteKolbService.java
@@ -0,0 +1,13 @@
+package com.tutor.ti_kolb.services;
+
+import com.tutor.ti_kolb.repositories.KolbRepository;
+
+public class TesteKolbService {
+
+    private final KolbRepository kolbRepository;
+
+    public TesteKolbService(KolbRepository kolbRepository) {
+        this.kolbRepository = kolbRepository;
+    }
+
+}
