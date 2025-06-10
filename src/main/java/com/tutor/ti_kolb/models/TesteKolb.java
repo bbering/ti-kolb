741a11a545e7c9f679cfd5dc249eff4c0695655a
bbering <bbering88@gmail.com>
2025-06-10 16:31:27 -0300

    refactor(TesteKolb): refatoração da entity para armazenar as respostas em forma de JSON, para evitar o crescimento exponencial de registros por criação de teste

--- src/main/java/com/tutor/ti_kolb/models/TesteKolb.java
+++ src/main/java/com/tutor/ti_kolb/models/TesteKolb.java
@@ -1,17 +1,13 @@
 package com.tutor.ti_kolb.models;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -33,10 +29,8 @@ public class TesteKolb {
     @Column(nullable = false)
     private UUID userId;
 
-    @ElementCollection
-    @CollectionTable(name = "respostas_kolb", joinColumns = @JoinColumn(name = "teste_id"))
-    @Column(name = "resposta")
-    private List<Integer> respostas;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String respostasJson;
 
     @Column(nullable = false)
     private String perfil; // assimilador, convergente...
