23df9e59f7ea97ca5e10155dbaa48ef15193b82f
bbering <bbering88@gmail.com>
2025-06-10 16:05:54 -0300

    feat(TesteKolbService): criação do metodo auxiliar para criar um teste de kolb

--- src/main/java/com/tutor/ti_kolb/services/TesteKolbService.java
+++ src/main/java/com/tutor/ti_kolb/services/TesteKolbService.java
@@ -1,5 +1,12 @@
 package com.tutor.ti_kolb.services;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import com.tutor.ti_kolb.dtos.KolbResultDTO;
+import com.tutor.ti_kolb.models.TesteKolb;
 import com.tutor.ti_kolb.repositories.KolbRepository;
 
 public class TesteKolbService {
@@ -8,6 +15,33 @@ public class TesteKolbService {
 
     public TesteKolbService(KolbRepository kolbRepository) {
         this.kolbRepository = kolbRepository;
+    }
+
+    public KolbResultDTO makeTest(UUID userUuid, List<Integer> answers) {
+        /*
+         * aqui é implementado o metodo que consumirá o json recebido via fila
+         */
+
+        // instanciar a entidade
+        TesteKolb testToSave = new TesteKolb();
+
+        // aqui será mockado um userID ainda não-real
+        testToSave.setId(userUuid);
+
+        // criação de uma variavel para capturar o momento em que o teste é respondido
+        // no horario local
+        LocalDateTime answerTime = LocalDateTime.now();
+        testToSave.setDataResposta(answerTime);
+
+        testToSave.setRespostas(answers);
+
+        // aqui o resultado do metodo auxiliar será passado e o perfil setado ao fim do
+        // cálculo
+        testToSave.setPerfil(null);
+
+        // montar o DTO para ser retornado ao criar um novo teste e evitar expor dados
+        // sensiveis
+        return new KolbResultDTO(testToSave.getUserId(), testToSave.getPerfil());
     }
 
 }
