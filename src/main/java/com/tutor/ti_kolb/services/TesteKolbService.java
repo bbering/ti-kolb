8dff95a80e66524b9f10976b5127e7c08f40fb60
bbering <bbering88@gmail.com>
2025-06-10 16:09:42 -0300

    feat(TesteKolbService): service elaborada com todos os metodos auxiliares necessarios para realizar um teste de kolb

--- src/main/java/com/tutor/ti_kolb/services/TesteKolbService.java
+++ src/main/java/com/tutor/ti_kolb/services/TesteKolbService.java
@@ -1,14 +1,16 @@
 package com.tutor.ti_kolb.services;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.stereotype.Service;
 
 import com.tutor.ti_kolb.dtos.KolbResultDTO;
 import com.tutor.ti_kolb.models.TesteKolb;
 import com.tutor.ti_kolb.repositories.KolbRepository;
 
+@Service
 public class TesteKolbService {
 
     private final KolbRepository kolbRepository;
@@ -37,11 +39,48 @@ public class TesteKolbService {
 
         // aqui o resultado do metodo auxiliar será passado e o perfil setado ao fim do
         // cálculo
-        testToSave.setPerfil(null);
+        testToSave.setPerfil(calcularPerfilKolb(answers));
+
+        kolbRepository.save(testToSave);
 
         // montar o DTO para ser retornado ao criar um novo teste e evitar expor dados
         // sensiveis
         return new KolbResultDTO(testToSave.getUserId(), testToSave.getPerfil());
+    }
+
+    public String calcularPerfilKolb(List<Integer> respostas) {
+        // a lista de respostas deve conter 48 entradas, (12 perguntas com 4
+        // alternativas, 12x4= 48)
+        if (respostas == null || respostas.size() != 48) {
+            throw new IllegalArgumentException("A lista de respostas deve conter exatamente 48 valores.");
+        }
+
+        int ce = 0; // concreto experiencial
+        int ro = 0; // reflexivo observador
+        int ac = 0; // abstrato conceitual
+        int ae = 0; // ativo experimental
+
+        // respostas organizadas em blocos de 4 alternativas
+        for (int i = 0; i < 48; i += 4) {
+            ce += respostas.get(i);
+            ro += respostas.get(i + 1);
+            ac += respostas.get(i + 2);
+            ae += respostas.get(i + 3);
+        }
+
+        int diffAE_RO = ae - ro;
+        int diffCE_AC = ce - ac;
+
+        // avaliação final do perfil do usuario
+        if (diffAE_RO >= 0 && diffCE_AC >= 0) {
+            return "Acomodador";
+        } else if (diffAE_RO >= 0 && diffCE_AC < 0) {
+            return "Convergente";
+        } else if (diffAE_RO < 0 && diffCE_AC >= 0) {
+            return "Divergente";
+        } else {
+            return "Assimilador";
+        }
     }
 
 }
