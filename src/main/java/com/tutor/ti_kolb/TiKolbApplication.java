db30d62d3d900c1e53b273b2f5fb6debd0ea3d58
bbering <bbering88@gmail.com>
2025-06-10 15:06:03 -0300

    init: adicionadas dependencias iniciais via spring initializr

---
+++ src/main/java/com/tutor/ti_kolb/TiKolbApplication.java
@@ -0,0 +1,13 @@
+package com.tutor.ti_kolb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiKolbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiKolbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutor/ti_kolb/TiKolbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutor.ti_kolb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiKolbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
