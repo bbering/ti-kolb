a0b15e49be400eb1a9270d983a4edf8a5667c1e2
bbering <bbering88@gmail.com>
2025-06-11 15:51:28 -0300

    fix(KolbResultDTO): pequena correçao no campo name para userID

--- src/main/java/com/tutor/ti_kolb/dtos/KolbResultDTO.java
+++ src/main/java/com/tutor/ti_kolb/dtos/KolbResultDTO.java
@@ -2,6 +2,5 @@ package com.tutor.ti_kolb.dtos;
 
 import java.util.UUID;
 
-public record KolbResultDTO(UUID name, String profile) {
-
+public record KolbResultDTO(UUID userId, String profile) {
 }
--- src/main/java/com/tutor/ti_kolb/dtos/KolbTestMessageDTO.java
+++
@@ -1,8 +0,0 @@
-package com.tutor.ti_kolb.dtos;
-
-import java.util.List;
-import java.util.UUID;
-
-public record KolbTestMessageDTO(UUID userId, List<Integer> answers) {
-
-}
