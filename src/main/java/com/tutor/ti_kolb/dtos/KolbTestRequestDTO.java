e557d8ab44cb70f04c14c206d54c73ecfd818175
bbering <bbering88@gmail.com>
2025-06-11 16:26:47 -0300

    feat(KolbTestRequestDTO): nova validação de campo name adicionada

--- src/main/java/com/tutor/ti_kolb/dtos/KolbTestRequestDTO.java
+++ src/main/java/com/tutor/ti_kolb/dtos/KolbTestRequestDTO.java
@@ -5,8 +5,7 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 import java.util.UUID;
 
-public record KolbTestRequestDTO(
-        @NotNull(message = "UserId não pode ser nulo") UUID userId,
-
-        @NotNull(message = "Respostas não podem ser nulas") @Size(min = 48, max = 48, message = "Devem ser exatamente 48 respostas") List<Integer> answers) {
+public record KolbTestRequestDTO(@NotNull(message = "UserId não pode ser nulo") UUID userId,
+                @NotNull(message = "Respostas não podem ser nulas") @Size(min = 48, max = 48, message = "Devem ser exatamente 48 respostas") List<Integer> answers,
+                @NotNull(message = "O nome não pode ser vazio") String name) {
 }
