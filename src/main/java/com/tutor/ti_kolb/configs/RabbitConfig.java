21d72b8f820d9f149e907764b516267ed35e8df1
bbering <bbering88@gmail.com>
2025-06-12 15:43:11 -0300

    feat(RabbitConfig): configuraçao do desserializador que converte json em object

--- src/main/java/com/tutor/ti_kolb/configs/RabbitConfig.java
+++ src/main/java/com/tutor/ti_kolb/configs/RabbitConfig.java
@@ -1,9 +1,13 @@
 package com.tutor.ti_kolb.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class RabbitConfig {
@@ -14,5 +18,23 @@ public class RabbitConfig {
     @Bean
     public Queue kolbTestQueue() {
         return new Queue(queueName, true);
+    }
+
+    // bean do jackson para auxiliar na conversao de byte para object
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    // configura o listener para converter o JSON recebido
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter messageConverter) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 }
