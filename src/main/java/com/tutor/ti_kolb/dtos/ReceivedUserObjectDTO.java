9111955129774153f2b7ec21b881ec5c423b16b0
bbering <bbering88@gmail.com>
2025-06-22 22:02:57 -0300

    docs(ReceivedUserObjectDTO): adicionada documentação para DTO que lida com eventos rabbit

--- src/main/java/com/tutor/ti_kolb/dtos/ReceivedUserObjectDTO.java
+++ src/main/java/com/tutor/ti_kolb/dtos/ReceivedUserObjectDTO.java
@@ -1,7 +1,11 @@
 package com.tutor.ti_kolb.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
-public record ReceivedUserObjectDTO(UUID userId, String name) {
+public record ReceivedUserObjectDTO(
+        @Schema(description = "Identificador único do usuário", example = "550e8400-e29b-41d4-a716-446655440000") UUID userId,
 
+        @Schema(description = "Nome do usuário", example = "Maria Oliveira") String name) {
 }
