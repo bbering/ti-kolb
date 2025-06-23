303989ebf71407070da1a8ab99803a0dabf85ecc
bbering <bbering88@gmail.com>
2025-06-22 22:02:30 -0300

    docs(KolbTestRequestDTO): adicionada documentação do DTO de requisição

--- src/main/java/com/tutor/ti_kolb/dtos/KolbTestRequestDTO.java
+++ src/main/java/com/tutor/ti_kolb/dtos/KolbTestRequestDTO.java
@@ -1,11 +1,16 @@
 package com.tutor.ti_kolb.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.util.List;
 import java.util.UUID;
 
-public record KolbTestRequestDTO(@NotNull(message = "UserId não pode ser nulo") UUID userId,
-                @NotNull(message = "Respostas não podem ser nulas") @Size(min = 48, max = 48, message = "Devem ser exatamente 48 respostas") List<Integer> answers,
-                @NotNull(message = "O nome não pode ser vazio") String name) {
+public record KolbTestRequestDTO(
+        @Schema(description = "Identificador único do usuário", example = "550e8400-e29b-41d4-a716-446655440000") @NotNull(message = "UserId não pode ser nulo") UUID userId,
+
+        @Schema(description = "Lista de 48 respostas do teste de Kolb", example = "[1, 2, 3, ..., 4]") @NotNull(message = "Respostas não podem ser nulas") @Size(min = 48, max = 48, message = "Devem ser exatamente 48 respostas") List<Integer> answers,
+
+        @Schema(description = "Nome do usuário que realizou o teste", example = "João da Silva") @NotNull(message = "O nome não pode ser vazio") String name) {
 }
